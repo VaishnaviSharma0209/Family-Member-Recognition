3bf66a4cc5dc1141ee01f5a7215e465ff2f61287
VaishnaviSharma0209 <72790064+VaishnaviSharma0209@users.noreply.github.com>
2021-02-23 17:16:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function me(){
+    document.getElementById("me").style.display="inline-block";
+    setTimeout(function(){
+        document.getElementById("me").style.display="none";
+    },3000)
+}
+function sis(){
+    document.getElementById("sis").style.display="inline-block";
+    setTimeout(function(){
+        document.getElementById("sis").style.display="none";
+    },3000)
+}
+function mom(){
+    document.getElementById("mom").style.display="inline-block";
+    setTimeout(function(){
+        document.getElementById("mom").style.display="none";
+    },3000)
+}
+function dad(){
+    document.getElementById("dad").style.display="inline-block";
+    setTimeout(function(){
+        document.getElementById("dad").style.display="none";
+    },3000)
+}
+function grandma(){
+    document.getElementById("grandma").style.display="inline-block";
+    setTimeout(function(){
+        document.getElementById("grandma").style.display="none";
+    },3000)
+}
+function grandpa(){
+    document.getElementById("grandpa").style.display="inline-block";
+    setTimeout(function(){
+        document.getElementById("grandpa").style.display="none";
+    },3000)
+}

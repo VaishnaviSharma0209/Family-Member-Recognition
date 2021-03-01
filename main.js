26d0f6c5cec1b129f7bed5f2fade99a485059cd5
VaishnaviSharma0209 <72790064+VaishnaviSharma0209@users.noreply.github.com>
2021-03-01 13:01:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -34,3 +34,25 @@ function grandpa(){
         document.getElementById("grandpa").style.display="none";
     },3000)
 }
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+})
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="image" src="'+data_uri+'">';
+    })
+}
+
+console.log("ml5 Version",ml5.version);
+classifier=ml5.imageClassifier('///',modelLoaded);
+function download(){
+    link=document.getElementById("link");
+    image=document.getElementById("image").src;
+    link.href=image;
+    link.click();
+}
